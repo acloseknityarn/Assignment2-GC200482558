13ffd7ef5a2d90777a89cb570dca23bfaadef30f
acloseknityarn <avverma19699@gmail.com>
2022-11-15 23:00:55 -0500

    Initial Commit: Created the Assignment 2 project shell structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment2gc200482558 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment2gc200482558 to javafx.fxml;
+    exports com.example.assignment2gc200482558;
+}
